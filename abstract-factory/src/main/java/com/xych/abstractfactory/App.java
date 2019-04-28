d633e337ece8633e14f9a45af93c55cbed5a9364
Lanboo <549434510@qq.com>
2019-04-28 22:29:31 +0800

    init abstract-factory

---
+++ abstract-factory/src/main/java/com/xych/abstractfactory/App.java
@@ -0,0 +1,11 @@
+package com.xych.abstractfactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
