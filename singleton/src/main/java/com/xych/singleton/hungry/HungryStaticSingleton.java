55a78528d51f603eb96cc2550e3871fcbb5b17d7
Lanboo <549434510@qq.com>
2019-04-30 15:26:32 +0800

    ADD hungry singleton

---
+++ singleton/src/main/java/com/xych/singleton/hungry/HungryStaticSingleton.java
@@ -0,0 +1,31 @@
+package com.xych.singleton.hungry;
+
+/**
+ * <pre>
+ * 饿汉式单例模式
+ * 
+ * 优点：写法简单，线程安全
+ * 缺点：类加载时就被初始化，不管你使用不使用，都会占用空间
+ * </pre>
+ * 
+ * @Author XYCH
+ * @CreateDate 2019年4月30日下午2:28:15
+ * @Since: JDK1.8
+ */
+public class HungryStaticSingleton {
+    private static HungryStaticSingleton singleton;
+    static {
+        singleton = new HungryStaticSingleton();
+    }
+
+    private HungryStaticSingleton() {
+        // 防止利用反射机制破坏单例模式
+        if(singleton != null) {
+            throw new RuntimeException("already exists");
+        }
+    }
+
+    public static HungryStaticSingleton getInstance() {
+        return singleton;
+    }
+}
