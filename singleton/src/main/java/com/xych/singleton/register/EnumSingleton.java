5d7b273b7dee1f266cf9d27d7c34835bd0fc6c2d
Lanboo <549434510@qq.com>
2019-05-06 16:06:21 +0800

    ADD 枚举方式实现单例

---
+++ singleton/src/main/java/com/xych/singleton/register/EnumSingleton.java
@@ -0,0 +1,18 @@
+package com.xych.singleton.register;
+
+public enum EnumSingleton {
+    INSTANCE;
+    private Object object;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public static EnumSingleton getInstance() {
+        return INSTANCE;
+    }
+}
