2d045d6e93d1d9b55bdc7dd4f025d61ea8af0710
Lanboo <549434510@qq.com>
2019-05-07 17:54:40 +0800

    UPDATE cglib proxy

--- proxy/src/test/java/com/xych/proxy/DaoCglibProxyTest.java
+++ proxy/src/test/java/com/xych/proxy/DaoCglibProxyTest.java
@@ -2,9 +2,8 @@ package com.xych.proxy;
 
 import org.junit.Test;
 
-import com.xych.proxy.cglibproxy.ADaoImpl;
+import com.xych.proxy.cglibproxy.Dao;
 import com.xych.proxy.cglibproxy.DaoCglibProxy;
-import com.xych.proxy.cglibproxy.IDao;
 import com.xych.proxy.cglibproxy.Log;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 public class DaoCglibProxyTest {
     @Test
     public void test1() {
-        DaoCglibProxy proxy = new DaoCglibProxy(new ADaoImpl(), new Log());
-        IDao dao = proxy.getInstance();
+        DaoCglibProxy proxy = new DaoCglibProxy(new Dao(), new Log());
+        Dao dao = proxy.getInstance();
         log.info("{}", dao);
         dao.save();
     }
