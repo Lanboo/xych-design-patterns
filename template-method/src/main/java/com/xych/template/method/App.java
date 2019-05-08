716874d671f2277dfc66bacab94dd32e358b9e88
Lanboo <549434510@qq.com>
2019-05-08 18:00:58 +0800

    INIT template-method pattern

---
+++ template-method/src/main/java/com/xych/template/method/App.java
@@ -0,0 +1,13 @@
+package com.xych.template.method;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
