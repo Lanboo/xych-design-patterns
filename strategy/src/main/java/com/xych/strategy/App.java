0d29ff85317c80738fdffe8f813c0217072650f8
Lanboo <549434510@qq.com>
2019-05-08 17:01:31 +0800

    UPDATE 添加注释

--- strategy/src/main/java/com/xych/strategy/App.java
+++ strategy/src/main/java/com/xych/strategy/App.java
@@ -1,10 +1,9 @@
 package com.xych.strategy;
 
-/**
- * Hello world!
- *
- */
 public class App {
+    /**
+     * 业务场景：该公司某个业务有很多上游渠道，交易完成后的异步通知大多都遵循同一套格式，但是厦门的回执不一样，需要特出处理
+     */
     public static void main(String[] args) {
         String xiamenNotifyStr = "this is xiamen notify str";
         String beijinNotifyStr = "this is beijing notify str";
