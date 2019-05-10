a82d67ca287704f3507310dd40145eb898e7d3f7
Lanboo <549434510@qq.com>
2019-05-10 17:41:29 +0800

    UPDATE App.main

--- observer/src/main/java/com/xych/observer/App.java
+++ observer/src/main/java/com/xych/observer/App.java
@@ -1,5 +1,6 @@
 package com.xych.observer;
 
+import com.xych.observer.jdkobserver.WeatherStation;
 import com.xych.observer.model.Weather;
 import com.xych.observer.simple.BaseDisplay;
 import com.xych.observer.simple.Observable;
@@ -7,6 +8,15 @@ import com.xych.observer.simple.Observable;
 public class App {
     public static void main(String[] args) {
         simpleObserver();
+        jdkObserver();
+    }
+
+    private static void jdkObserver() {
+        WeatherStation weatherStation = new WeatherStation();
+        weatherStation.addObserver(new com.xych.observer.jdkobserver.BaseDisplay());
+        weatherStation.addObserver(new com.xych.observer.jdkobserver.BaseDisplay());
+        weatherStation.setWeather(getWeather());
+        weatherStation.publish();
     }
 
     private static void simpleObserver() {
